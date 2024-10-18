b793471faf5c70e50e3d2d9755deef030ee56f60
Julian Brown <julian.brown@cpanel.net>
2024-10-18 14:27:23 -0500

    ZC-12253: Initial Release

---
+++ debify/debian/override_dh_auto_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source debian/vars.sh
+
+set -x
+
+export php_version=`echo "$php_parent" | perl -pe '$_ =~ s/^ea-php(\d)(\d)$/$1.$2/'`
+
+# The module itself
+install -d -m 755 $DEB_INSTALL_ROOT${php_extdir}
+install -d -m 755 $DEB_INSTALL_ROOT${php_docdir}
+
+install -m 755 ioncube_loader_lin_${php_version}.so $DEB_INSTALL_ROOT${php_extdir}
+install -m 644 LICENSE.txt $DEB_INSTALL_ROOT${php_docdir}
+install -m 644 README.txt $DEB_INSTALL_ROOT${php_docdir}
+
+# The ini snippet
+install -d -m 755 $DEB_INSTALL_ROOT${php_inidir}
+cat > $DEB_INSTALL_ROOT${php_inidir}/${inifile} <<EOF
+; Enable v14 IonCube Loader extension module
+zend_extension="${php_extdir}/ioncube_loader_lin_${php_version}.so"
+EOF
+
